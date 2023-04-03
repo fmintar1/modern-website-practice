d31b6e4ed2e30c936edb727b009d761b701b48fc
fmintar1 <fmintar1@gmail.com>
2023-04-02 20:28:49 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h1>
+        Testing 1, 2, 3
+      </h1>
+    </div>
+  )
+}
+
+export default App
