a6617d5d132c3895d68cd2ac633b376a639791a9
fmintar1 <fmintar1@gmail.com>
2023-04-02 20:31:54 -0400

    added style.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
-      <h1>
-        Testing 1, 2, 3
-      </h1>
+      <div>
+        
+      </div>
     </div>
   )
 }
